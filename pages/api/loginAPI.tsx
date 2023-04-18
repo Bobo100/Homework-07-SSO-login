db22117cfa8ab865bd0abf037bd7e2bb27cf3547
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-18 15:14:26 +0800

    Update loginAPI.tsx

--- pages/api/loginAPI.tsx
+++ pages/api/loginAPI.tsx
@@ -1,46 +1,98 @@
+// import axios from "axios";
+// import type { NextApiRequest, NextApiResponse } from "next";
+
+// const verifyRecaptcha = async (token: string) => {
+//     const secretKey = process.env.RECAPTCHA_SECRETKEY;
+
+//     const verificationUrl =
+//         `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+
+//     return await axios.post(verificationUrl)
+// };
+
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<any>
+// ) {
+//     try {
+//         const token = req.body.gRecaptchaToken;
+
+//         // Recaptcha response
+//         const response = await verifyRecaptcha(token);
+
+//         // Checking if the reponse sent by reCaptcha success or not and if the score is above 0.5
+//         // In ReCaptcha v3, a score sent which tells if the data sent from front end is from Human or from Bots. If score above 0.5 then it is human otherwise it is bot
+//         // For more info check, https://developers.google.com/recaptcha/docs/v3
+//         // ReCaptcha v3 response, {
+//         //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
+//         //     "score": number             // the score for this request (0.0 - 1.0)
+//         //     "action": string            // the action name for this request (important to verify)
+//         //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+//         //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+//         //     "error-codes": [...]        // optional
+//         //   }
+
+//         if (response.data.success && response.data.score >= 0.5) {
+//             //INSERT API/LOGIC for saving data once the validation is complete
+//             return res.json({
+//                 status: "success",
+//                 message: response.data.score,
+//             });
+//         } else {
+//             return res.json({
+//                 status: "error 2",
+//                 message: { token, response },
+//             });
+//         }
+//     } catch (error) {
+//         res.json({
+//             status: "error 1",
+//             message: error,
+//         });
+//     }
+// }
+
+
 import axios from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const verifyRecaptcha = async (token: string) => {
+interface RecaptchaResponse {
+    success: boolean;
+    score: number;
+    action: string;
+    challenge_ts: string;
+    hostname: string;
+}
+
+const verifyRecaptcha = async (token: string): Promise<RecaptchaResponse> => {
     const secretKey = process.env.RECAPTCHA_SECRETKEY;
+    const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
 
-    var verificationUrl =
-        "https://www.google.com/recaptcha/api/siteverify?secret=" +
-        secretKey +
-        "&response=" +
-        token;
-
-
-    return await axios.post(verificationUrl);
+    try {
+        const response = await axios.post(verificationUrl);
+        return response.data as RecaptchaResponse;
+    } catch (error) {
+        console.error(`reCAPTCHA verification failed: ${error}`);
+        return { success: false, score: 0, action: "", challenge_ts: "", hostname: "" };
+    }
 };
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<any>
+    res: NextApiResponse
 ) {
     try {
         const token = req.body.gRecaptchaToken;
 
-        // Recaptcha response
+        // Verify Recaptcha response
         const response = await verifyRecaptcha(token);
 
-        // Checking if the reponse sent by reCaptcha success or not and if the score is above 0.5
-        // In ReCaptcha v3, a score sent which tells if the data sent from front end is from Human or from Bots. If score above 0.5 then it is human otherwise it is bot
-        // For more info check, https://developers.google.com/recaptcha/docs/v3
-        // ReCaptcha v3 response, {
-        //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
-        //     "score": number             // the score for this request (0.0 - 1.0)
-        //     "action": string            // the action name for this request (important to verify)
-        //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-        //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
-        //     "error-codes": [...]        // optional
-        //   }
-
-        if (response.data.success && response.data.score >= 0.5) {
+        // Check if the response sent by reCaptcha is successful and the score is above 0.5
+        if (response.success && response.score >= 0.5) {
             //INSERT API/LOGIC for saving data once the validation is complete
             return res.json({
                 status: "success",
-                message: response.data.score,
+                message: response.score,
             });
         } else {
             return res.json({

77e744d70e76e271c904350ff5dc830a3e6b5aac
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-19 13:29:51 +0800

    更新key

--- next.config.js
+++ next.config.js
@@ -4,13 +4,4 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  env: {
-    apiKey: "AIzaSyA-cSOXbjbJpreIvO8klWFgnTL5cXL7gDI",
-    authDomain: "sso-login-eb4e3.firebaseapp.com",
-    projectId: "sso-login-eb4e3",
-    storageBucket: "sso-login-eb4e3.appspot.com",
-    messagingSenderId: "229894979483",
-    appId: "1:229894979483:web:bd02c16c4806833b6a6b3e",
-    measurementId: "G-8226WB0ZN",
-  },
 };
